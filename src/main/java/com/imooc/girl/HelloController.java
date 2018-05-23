0c380fd5b1440925d8339efc66395a0163f91000
momoka <454768497@qq.com>
2018-05-23 16:29:25 +0800

    从入门到弃坑

--- src/main/java/com/imooc/girl/HelloController.java
+++ src/main/java/com/imooc/girl/HelloController.java
@@ -1,5 +1,7 @@
 package com.imooc.girl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private GirlProperties girlProperties;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String say(){
-        return "Hello Spring Boot";
+        return girlProperties.getCupSize();
     }
 }

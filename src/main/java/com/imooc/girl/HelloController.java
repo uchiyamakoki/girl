1eb156a95acdf06ecb7a71d55c4000285b71d64c
momoka <454768497@qq.com>
2018-05-27 21:19:38 +0800

    springboot继续弃坑中

--- src/main/java/com/imooc/girl/HelloController.java
+++ src/main/java/com/imooc/girl/HelloController.java
@@ -2,18 +2,22 @@ package com.imooc.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
+@ResponseBody
+@RequestMapping("/hello")
 public class HelloController {
 
     @Autowired
     private GirlProperties girlProperties;
-
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String say(){
-        return girlProperties.getCupSize();
+    //@PathVariable("id") Integer id 对应 /say/{id}
+    //@RequestMapping(value = {"/say","/hi"},method = RequestMethod.GET)
+    @GetMapping(value = "/say")
+    public String say(@RequestParam(value = "id",required = false,defaultValue = "0") Integer id){
+        //return girlProperties.getCupSize();
+        //return "index";
+        return "id:"+id;
     }
 }
